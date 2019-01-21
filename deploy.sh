eac78212b6cc160bb29a1bbfde0d58fa422ec0fc
Puck Wang <s9801077@gmail.com>
2019-01-21 10:31:28 +0800

    Publishing to github (deploy.sh)

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,10 @@ echo "# Deleting old publication"
 rm -rf dist
 mkdir dist
 git worktree prune
-rm -rf .git/worktrees/dist/
+rm -rf .git/worktrees/gh-pages/
 
 echo "# Checking out public branch into dist"
-git worktree add -B dist dist public/master
-
-echo "# Removing existing files"
-rm -rf dist/*
+git worktree add -B gh-pages dist public/gh-pages
 
 echo "# Generating site"
 yarn build --no-clean
@@ -18,5 +15,5 @@ yarn build --no-clean
 echo "# Updating public branch"
 echo "tool.puckwang.com" > dist/CNAME
 
-cd public && git add --all && git commit -m "Publishing to github (deploy.sh)"
-git push public master
+cd dist && git add --all && git commit -m "Publishing to github (deploy.sh)"
+git push public gh-pages
